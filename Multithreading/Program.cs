341e48e82a30c85b7f895eaf15c05f46e99b5568
calin <calinoiu.alexandru@agilefreaks.com>
2015-10-31 18:13:25 +0200

    Initial check-in of module Multithreading

---
+++ Multithreading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Multithreading
+{
+	public class Program
+	{
+		static volatile bool cancelThread = false;
+
+		static void Main(string[] args)
+		{
+			Thread th = new Thread(SayHello);
+			th.Name = "Hello";
+			th.Priority = ThreadPriority.Normal;
+			th.Start("Calin");
+			Console.ReadLine ();
+
+			cancelThread = true;
+			th.Join ();
+			Console.WriteLine ("I am the main threat");
+			Console.ReadLine ();
+		}
+
+		private static void SayHello (object name)
+		{
+			while (!cancelThread) 
+			{
+				Console.WriteLine ("Hello " + (string)name);
+			}
+		}
+	}
+}
+
